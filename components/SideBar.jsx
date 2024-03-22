999f9c036e0a9eaa464d4b38d72f8170291b703c
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2024-03-22 13:06:49 +0000

    sidebar fixed

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,11 +1,38 @@
 "use client"
+
+import { useCallback, useEffect, useRef } from "react"
+
 const SideBar = ({ sidebar, setsidebar }) => {
+
+    const toggleNav = function () {
+        setsidebar(!sidebar)
+    }
+
+    const menuRef = useRef();
+
+    const closeOpenMenus = useCallback(
+        (e) => {
+            if (
+                menuRef.current &&
+                sidebar &&
+                !menuRef.current.contains(e.target)
+            ) {
+                setsidebar(false);
+            }
+        },
+        [sidebar]
+    );
+
+    useEffect(() => {
+        document.addEventListener("mousedown", closeOpenMenus);
+    }, [closeOpenMenus]);
+
     return (
         <>
 
-            <aside className={`absolute top-0 left-0 h-full transition-all duration-1000 ease-in-out ${!sidebar ? "md:w-[25vw] w-[60vw] -translate-x-full" : "md:w-[40vw] w-[60vw] -translate-x-1"}`}>
+            <aside ref={menuRef} className={`fixed top-0 left-0 h-full transition-all duration-1000 ease-in-out ${!sidebar ? "md:w-[25vw] w-[60vw] -translate-x-full" : "md:w-[40vw] w-[60vw] -translate-x-1"}`}>
                 <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <div className="w-4 text-xxl fixed top-1/2 right-[-15px]"><button className='w-5 h-10 text-gray-50 bg-gray-700' onClick={()=>setsidebar(!sidebar)}>{sidebar ? "<":">"}</button></div>
+                    <div className="w-4 text-xxl fixed top-1/2 right-[-15px]"><button className='w-5 h-10 text-gray-50 bg-gray-700' onClick={toggleNav}>{sidebar ? "<":">"}</button></div>
                     <ul className="space-y-2 font-medium">
                         <li>
                             <div className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-700">
